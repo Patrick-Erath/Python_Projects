8b23f74f97c08f9d4612455eac7a4b9189e62f74
patrick <patrick.erath@mail.mcgill.ca>
2019-03-18 17:11:08 -0400

    updated

--- RedditAPI/1.py
+++ RedditAPI/1.py
@@ -3,9 +3,9 @@ import praw
 #Create reddit instance
 reddit = praw.Reddit(client_id='wqrrsTJXlIrG7A',
 					client_secret='g72L_jWvbJEduw2Ho_gnX1P4a1g', 
-					password='lolmaster',
+					password='PASSWORD',
 					user_agent='Patrick', 
-					username='me_using_reddit')
+					username='USERNAME')
 
 subreddit=reddit.subreddit('python')
 hot_python = subreddit.hot(limit=3) # Returns submission objects
