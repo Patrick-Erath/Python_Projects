a7ab598ba117014694cd79c14eabf5e3a4fb7dba
patrick <patrick.erath@mail.mcgill.ca>
2019-03-18 17:30:29 -0400

    syntax update

--- RedditAPI/comments.py
+++ RedditAPI/comments.py
@@ -6,7 +6,7 @@ reddit = praw.Reddit(client_id='wqrrsTJXlIrG7A',
 					client_secret='g72L_jWvbJEduw2Ho_gnX1P4a1g', 
 					password='PASSWORD',
 					user_agent='Patrick', 
-					username='username')
+					username='USERNAME')
 
 subreddit = reddit.subreddit('python')
 hot_python = subreddit.hot(limit=3)
