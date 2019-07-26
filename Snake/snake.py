36689a7d1c7169f4a88782045967b849479595e6
patrick <patrick.erath@mail.mcgill.ca>
2019-07-26 10:40:30 +0200

    added moving functionality and eatting fruit functionality to snake:

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,5 +1,3 @@
-print("niet")
-
 import pygame
 import numpy as np
 from random import randrange
@@ -8,14 +6,9 @@ class Snake:
 	def __init__(self):
 		self.display_width = 400
 		self.size = 10;
-		self.x = randrange(5, self.display_width)
-		self.y = randrange(5, self.display_width)
 		# need vector of x,y 
 		self.array = np.array([
-			[self.x, self.y],
-			[self.x-1, self.y],
-			[self.x-2, self.y],
-			[self.x-3, self.y]
+			[randrange(5, self.display_width), randrange(5, self.display_width)],
 		])
 
 	def draw(self):
@@ -33,7 +26,7 @@ class Snake:
 
 		FRUIT_PLACE = False
 		#while !FRUIT_PLACE:
-			#for x,y in array:
+			# for x,y in array:
 				#if x+self.size==
 
 		while QUIT==False:
@@ -42,14 +35,72 @@ class Snake:
 					QUIT = True
 					pygame.quit()
 					quit()
-				#reset
+				# reset
 				gameDisplay.fill(white)
-				#draw snake
+
+				#  draw snake
 				for x,y in self.array:
 					pygame.draw.rect(gameDisplay, black, [x, y, self.size, self.size])
-				#draw fruit
+
+				# draw fruit
 				pygame.draw.rect(gameDisplay, blue, [x_fruit, y_fruit, self.size, self.size])
+
+				# ensure fruit not in snake coordinates
+				for x,y in self.array:
+					is_inside = False;
+					if(abs(x - x_fruit) < self.size and abs(y - y_fruit) < self.size):
+						print(x)
+						print(x_fruit)
+						# fruit  is inside snake coordinate, make new fruit
+						while(True):
+							x_fruit = randrange(1, self.display_width)
+							y_fruit = randrange(1, self.display_width)
+							if(abs(x - x_fruit) < self.size or abs(y - y_fruit) < self.size):
+								break
+
+
+				# get move events
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_RIGHT:
+						move = 'right'
+						self.moveSnake(move)
+					elif event.key == pygame.K_LEFT:
+						move = "left"
+						self.moveSnake(move)
+					elif event.key == pygame.K_UP:
+						move = "up"
+						self.moveSnake(move)
+					elif event.key == pygame.K_DOWN:
+						move = "down"
+						self.moveSnake(move)
+
+
 				pygame.display.update()
+
+	def moveSnake(self, event):
+		# grab head
+		new_x = self.array[0][0]
+		new_y = self.array[0][1]
+
+		# make sure within boundaries
+		if(new_x < self.display_width and new_y < self.display_width):
+			for x,y in self.array:
+				if(event == 'right'):
+					new_x = x + self.size
+				if(event == 'left'):
+					new_x = x - self.size
+				if (event == 'down'):
+					new_y = y + self.size
+				if (event == 'up'):
+					new_y = y - self.size
+
+		# update
+		self.array[0][0] = new_x
+		self.array[0][1] = new_y
+
+		#for x, y in self.array:
+			#pygame.draw.rect(display, color, [x, y, self.size, self.size])
+			# check if ate fruit
 
 if __name__ == '__main__':
 	snake = Snake()
